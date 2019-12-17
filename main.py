ba8b4bfac897561ac36234ca4bfad498ad72f1d5
prouget <prouget2000@yahoo.fr>
2019-12-17 15:44:36 +0100

    Splash Screen

--- main.py
+++ main.py
@@ -2,12 +2,36 @@ import sys
 import sqlite3
 
 from PyQt5 import QtWidgets, uic, QtGui
+from PyQt5.QtWidgets import QMessageBox
 
-from generator import pw_gen
+from generator import pw_gen, pw_gen_num
+
+from drop import TransferData
 
 
 app = QtWidgets.QApplication([])
 w = uic.loadUi("pass.ui")
+splash = uic.loadUi("splash.ui")
+
+database = "pass.pdf"
+
+# ----- SPLASH -----
+
+def connect():
+    pw = 'test'
+    p = splash.lineEdit_Con.text()
+    if p == pw:
+        splash.close()
+        w.show()
+    else:
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Critical)
+        msg.setText("!! Erreur !!")
+        msg.setInformativeText('Mauvais mot de passe')
+        msg.setWindowTitle("Erreur")
+        msg.exec_()
+
+# ------------------
 
 def clearData():
     while (w.tableWidget.rowCount()>0):
@@ -15,7 +39,7 @@ def clearData():
 
 def dataConnect():
 
-    connexion = sqlite3.connect("pass.db")
+    connexion = sqlite3.connect(database)
     cursor = connexion.cursor()
 
     cursor.execute('''
@@ -25,13 +49,12 @@ def dataConnect():
                     identifiant TEXT,
                     password TEXT)
                 ''')
-
     connexion.close()
 
 def showData():
     clearData()
 
-    connexion = sqlite3.connect("pass.db")
+    connexion = sqlite3.connect(database)
     cursor = connexion.cursor()
 
     cursor.execute("SELECT domaine, identifiant, password FROM passid")
@@ -44,39 +67,40 @@ def showData():
             w.tableWidget.setItem(idx, column_number, cell)
 
     header = w.tableWidget.horizontalHeader()
-    header.setSectionResizeMode(QtWidgets.QHeaderView.ResizeToContents)
+    # header.setSectionResizeMode(QtWidgets.QHeaderView.ResizeToContents)
     header.setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
     header.setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)     
     header.setSectionResizeMode(2, QtWidgets.QHeaderView.Stretch)
 
 def addRecord():
+
     d = w.lineEdit_Domain.text()
     i = w.lineEdit_Id.text()
     p = w.lineEdit_Pass.text()
     donnees = (d, i, p)
-    print(donnees)
 
-    connexion = sqlite3.connect("pass.db")
+    connexion = sqlite3.connect(database)
     cursor = connexion.cursor()
 
     cursor.execute('''INSERT INTO passid (domaine, identifiant, password) VALUES (?, ?, ?)''', (donnees))
     connexion.commit() 
 
-def removeRecord():
+    showData()
 
-    # row = w.tableWidget.currentRow()
-    # row = row+1
+def removeRecord():
 
     row = w.tableWidget.item(getSelectedRowId(), 0).text()
     print(row)
 
-    connexion = sqlite3.connect("pass.db")
+    connexion = sqlite3.connect(database)
     cursor = connexion.cursor()
 
     query = "DELETE FROM passid WHERE domaine = '%s';" % row
     cursor.execute(query)
     print('ok')
     connexion.commit()
+
+    showData()
 
 def getSelectedRowId():
     return w.tableWidget.currentRow()
@@ -85,22 +109,59 @@ def getSelectedRowId():
 
 def passW():
     l = w.spinBox_Length.text()
-    p = pw_gen(int(l))
+    c = w.checkBox.isChecked()
+    if c:
+        p = pw_gen_num(int(l))
+    else:
+        p = pw_gen(int(l))
     w.lineEdit_Password.setText(p)
 
 def validPass():
     gpass = w.lineEdit_Password.text()
     w.lineEdit_Pass.setText(gpass)
 
+# ----- SYNCHRO -----
+def uploadDrop():
+    w.label_save.setText('-')
+    access_token = 's_5yuTbKn0AAAAAAAAAAWi51bnBlfTpw7n5T-ZVyP2InYuP_kqBrWMGX55OmZJnH'
+    transferData = TransferData(access_token)
+
+    file_from = 'pass.pdf'
+    file_to = '/API/pass.pdf'  # The full path to upload the file to, including the file name
+
+    # API DROPBOX v2
+    transferData.upload_file(file_from, file_to)
+
+    w.label_save.setText('Envoyé !')
+
+def downloadDrop():
+    w.label_get.setText('-') 
+    access_token = 's_5yuTbKn0AAAAAAAAAAWi51bnBlfTpw7n5T-ZVyP2InYuP_kqBrWMGX55OmZJnH'
+    transferData = TransferData(access_token)
+
+    # API DROPBOX v2
+    transferData.download_File()   
+
+    w.label_get.setText('Téléchargé !') 
+
 # ----- ACTIONS -----
 
 w.pushButton_Open.clicked.connect(showData)
+w.actionOuvrir_la_Base.triggered.connect(showData)
 w.pushButton_Add.clicked.connect(addRecord)
 w.pushButton_Remove.clicked.connect(removeRecord)
+
+w.pushButton_Get_Online.clicked.connect(downloadDrop)
+w.actionRecuperation.triggered.connect(downloadDrop)
+w.pushButton_Save_Online.clicked.connect(uploadDrop)
+w.actionSauvegarde.triggered.connect(uploadDrop)
 
 w.pushButton_Generate.clicked.connect(passW)
 w.pushButton_Validate.clicked.connect(validPass)
 
+splash.pushButton_Con.clicked.connect(connect)
+
 dataConnect()
-w.show()
+splash.show()
+# w.show()
 app.exec_()
