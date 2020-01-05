fff2763d7a319c62843a380dfd7635ddb222b03f
prouget <prouget2000@yahoo.fr>
2020-01-05 16:20:49 +0100

    Add files via upload
    small changes

--- generator.py
+++ generator.py
@@ -3,6 +3,10 @@ import random
 
 signe = '/?#><!-'
 
-def pw_gen(size = 8, chars=string.ascii_letters + string.digits + signe):    #+ string.punctuation
+def pw_gen(size = 8, chars=string.ascii_letters + string.digits + signe):
+    p = ''.join(random.choice(chars) for c in range(size))
+    return p
+
+def pw_gen_num(size = 8, chars=string.digits):
     p = ''.join(random.choice(chars) for c in range(size))
     return p
